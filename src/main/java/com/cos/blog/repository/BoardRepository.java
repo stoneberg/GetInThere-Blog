e72dadf7ed7f13b00a4313594c1a9e2a7a54d9c6
stoneberg <stoneberg73@gmail.com>
2020-07-30 13:56:18 +0900

    left join fetch by id

--- src/main/java/com/cos/blog/repository/BoardRepository.java
+++ src/main/java/com/cos/blog/repository/BoardRepository.java
@@ -5,16 +5,17 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.cos.blog.model.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Integer> {
 
-    @EntityGraph(attributePaths = { "user", "replies", "replies.user" })
-    @Query("select DISTINCT b from Board b")
-    Optional<Board> findEntityGraphBoardById(int id);
+    @EntityGraph(attributePaths = { "user", "replies", "replies.user" }, type = EntityGraph.EntityGraphType.LOAD)
+    @Query("select DISTINCT b from Board b where b.id=:id")
+    Optional<Board> findEntityGraphBoardById(@Param("id") Integer id);
 
-    @Query("select DISTINCT b from Board b join fetch b.user join fetch b.replies r join fetch r.user")
-    Optional<Board> findFetchJoinBoardById(int id);
+    @Query("select DISTINCT b from Board b left join fetch b.user left join fetch b.replies r left join fetch r.user where b.id=:id")
+    Optional<Board> findFetchJoinBoardById(@Param("id") Integer id);
 
 }
